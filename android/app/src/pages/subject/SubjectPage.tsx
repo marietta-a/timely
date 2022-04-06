d358808a10929482e6445489bf815d2ac4ce779d
akumbom <akumbom5ma@gmail.com>
2022-04-06 16:00:34 +0100

    (wip): setting up student page

---
+++ android/app/src/pages/subject/SubjectPage.tsx
@@ -0,0 +1,22 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Component } from "react";
+import { View } from "react-native";
+import { Subject } from "../../models/Subject";
+
+const student : Subject[] = [
+    {
+        Name: "Maths",
+        Color: "#dddddd",
+        Teacher: "p2"
+    }
+]
+class SubjectPage extends Component{
+    render(){
+        return(
+            <View>
+      
+            </View>
+        )
+    }
+}
